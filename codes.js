f75d82b23106dcf748112ae6f5bddd33768da2dc
Anthony Destefano <anthony.destefano@gmail.com>
2015-09-19 00:44:23 -0400

    Happy Birthday

---
+++ codes.js
@@ -0,0 +1,59 @@
+
+// headers
+var HDRS = [];
+HDRS[0] = new HTTP("100", "Continue");
+HDRS[1] = new HTTP("101", "Switching Protocols");
+HDRS[2] = new HTTP("102", "Processing");
+HDRS[3] = new HTTP("103", "Unassigned");
+HDRS[4] = new HTTP("200", "OK");
+HDRS[5] = new HTTP("201", "Created");
+HDRS[6] = new HTTP("202", "Accepted");
+HDRS[7] = new HTTP("203", "Non-Authoritative Information");
+HDRS[8] = new HTTP("204", "No Content");
+HDRS[9] = new HTTP("205", "Reset Content");
+HDRS[10] = new HTTP("206", "Partial Content");
+HDRS[11] = new HTTP("207", "Multi-Status");
+HDRS[12] = new HTTP("208", "Already Reported");
+HDRS[13] = new HTTP("209", "Unassigned");
+HDRS[14] = new HTTP("226", "IM Used");
+HDRS[15] = new HTTP("227", "Unassigned");
+HDRS[16] = new HTTP("300", "Multiple Choices");
+HDRS[17] = new HTTP("301", "Moved Permanently");
+HDRS[18] = new HTTP("302", "Found");
+HDRS[19] = new HTTP("303", "See Other");
+HDRS[20] = new HTTP("304", "Not Modified");
+
+
+
+G.HDRS[0] = new G.HTTP("100", "Continue");
+G.HDRS[1] = new G.HTTP("101", "Switching Protocols");
+G.HDRS[2] = new G.HTTP("102", "Processing");
+G.HDRS[3] = new G.HTTP("103", "Unassigned");
+G.HDRS[4] = new G.HTTP("200", "OK");
+G.HDRS[5] = new G.HTTP("201", "Created");
+G.HDRS[6] = new G.HTTP("202", "Accepted");
+G.HDRS[7] = new G.HTTP("203", "Non-Authoritative Information");
+G.HDRS[8] = new G.HTTP("204", "No Content");
+G.HDRS[9] = new G.HTTP("205", "Reset Content");
+G.HDRS[10] = new G.HTTP("206", "Partial Content");
+G.HDRS[11] = new G.HTTP("207", "Multi-Status");
+G.HDRS[12] = new G.HTTP("208", "Already Reported");
+G.HDRS[13] = new G.HTTP("209", "Unassigned");
+G.HDRS[14] = new G.HTTP("226", "IM Used");
+G.HDRS[15] = new G.HTTP("227", "Unassigned");
+G.HDRS[16] = new G.HTTP("300", "Multiple Choices");
+G.HDRS[17] = new G.HTTP("301", "Moved Permanently");
+G.HDRS[18] = new G.HTTP("302", "Found");
+G.HDRS[19] = new G.HTTP("303", "See Other");
+G.HDRS[20] = new G.HTTP("304", "Not Modified");
+
+G.HDRS[21] = new G.HTTP("304", "Not Modified");
+G.HDRS[22] = new G.HTTP("304", "Not Modified");
+G.HDRS[23] = new G.HTTP("304", "Not Modified");
+G.HDRS[24] = new G.HTTP("304", "Not Modified");
+G.HDRS[25] = new G.HTTP("304", "Not Modified");
+G.HDRS[26] = new G.HTTP("304", "Not Modified");
+G.HDRS[27] = new G.HTTP("304", "Not Modified");
+G.HDRS[28] = new G.HTTP("304", "Not Modified");
+G.HDRS[29] = new G.HTTP("304", "Not Modified");
+G.HDRS[30] = new G.HTTP("304", "Not Modified");
